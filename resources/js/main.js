4f71d7ffa3202bd188286c0ee1e40018e2d4a39d
Anthony Williams <aaw268@nau.edu>
2019-05-30 11:45:07 -0700

    slick caresoul works as intended. Work on nav bar

---
+++ resources/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.carousel').slick({
+    autoplay: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows: false,
+  });
+});
